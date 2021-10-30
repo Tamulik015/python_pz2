199c74eff7bd582688ddfe64b038ca37ad2c28ee
Tamik Tomaev <t.tomaev2016@yandex.ru>
2021-10-30 13:04:27 +0300

    hw2

--- task4.py
+++ task4.py
@@ -1,6 +1,7 @@
 str = input('Введите слова: ')
 array = str.split(' ')
 
+
 for elem in array:
     if len(elem) > 10:
         print(f'{array.index(elem)}', elem[0:4])
--- task5.py
+++ task5.py
@@ -1,6 +1,7 @@
 array = [7, 5, 3, 3, 2]
 rating = int(input('Введите рейтинг: '))
 
+
 for elem in array:
     if rating > elem:
         array.insert(array.index(elem), rating)
